46858b04c0fe5a76eb40fd55cbba849b048fa069
Matthias <mail@matthiasbock.net>
2017-07-17 22:16:26 +0200

    Exported SVG renders drill operation correctly in browser

--- job.py
+++ job.py
@@ -37,10 +37,19 @@ class Job:
     # Export model as SVG
     #
     def exportSVG(self, filename):
+        # open file for writing
         f = open(filename, "w")
-        f.write('<svg width="'+str(self.sizeX)+'" height="'+str(self.sizeY)+'">\n')
+
+        # write SVG preamble
+        f.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
+        f.write('<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" ')
+        f.write('width="'+str(self.sizeX)+'" height="'+str(self.sizeY)+'">\n')
+
+        # export all toolpath steps to SVG
         for step in self.toolpath:
             f.write(step.exportSVG())
+
+        # write SVG epilogue
         f.write('</svg>\n')
         f.close()
 
--- step.py
+++ step.py
@@ -9,4 +9,5 @@ class Step:
         self.type = type
 
     def exportSVG(self):
-        return '<circle x="'+str(self.x)+'" y="'+str(self.y)+'" r="'+str(self.tool.getDiameter())+'"/>\n'
+        style = 'style="fill:white; stroke:red; stroke-width:1; stroke-dasharray:1,1;"'
+        return '<circle '+style+' cx="'+str(self.x)+'" cy="'+str(self.y)+'" r="'+str(self.tool.getDiameter())+'"/>\n'
